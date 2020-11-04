85628930ff1957d38186e4040b26f7b620d55d1c
Bas Conijn <bc@batenburgtechniek.onmicrosoft.com>
2020-11-04 12:12:19 +0100

    Added sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgxsModule } from '@ngxs/store';
 
@@ -30,6 +31,7 @@ import { RepairsComponent } from './repairs/repairs.component';
     }),
     MatButtonModule,
     MatIconModule,
+    MatSidenavModule,
     MatToolbarModule,
   ],
   providers: [],
